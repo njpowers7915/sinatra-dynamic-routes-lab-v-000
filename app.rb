1ad8fdb50b922c658a9af2fa8a7da5dcffb8b760
Nicholas Powers <njpowers7915@gmail.com>
2018-06-03 02:14:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,13 +4,13 @@ class App < Sinatra::Base
   # Write your code here!
   
   get 'reversename/:name'
-  
+    @name = params[:name]
   end
   
   get '/square/:number'
   
   end
   
-  get '/'
+  get '/say/:number/:phrase'
   
 end
