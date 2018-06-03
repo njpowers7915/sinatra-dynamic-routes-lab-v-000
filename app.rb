83c42d563194cc8b0eb6e564cf823ba155e49a37
Nicholas Powers <njpowers7915@gmail.com>
2018-06-03 02:34:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -33,5 +33,9 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2'
+    @operation = params[:operation]
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+    "#{@number1.to_i @operation @number2.to_i"
     
 end
