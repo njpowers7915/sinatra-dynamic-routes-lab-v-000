8ed5f98bbad4bb63b587e2cff5525f1f3ca8dda1
Nicholas Powers <njpowers7915@gmail.com>
2018-06-03 02:13:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  
+  get 'reversename/:name'
+  
+  end
+  
+  get '/square/:number'
+  
+  end
+  
+  get '/'
+  
 end
