0d63a2aca0b3b9c164304846cd12171555638d9b
Nicholas Powers <njpowers7915@gmail.com>
2018-06-03 02:42:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -42,6 +42,8 @@ class App < Sinatra::Base
       "#{@number1.to_i - @number2.to_i}"
     elsif @operation == "multiply"
       "#{@number1.to_i * @number2.to_i}"
+    elsif @operation == "divide"
+      "#{@number1.to_i / @number2.to_i}"
   end
     
 end
